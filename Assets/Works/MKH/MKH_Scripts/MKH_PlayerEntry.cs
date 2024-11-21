c692b1fa57adbac0b94f0e91ae89a828587fc02e
MoonKyoungHun <lsf8355@gmail.com>
2024-11-21 16:14:01 +0900

    Fix : 게임 대기 창 수정
    대기창에 플레이어 나오게 만들어 놓음

--- Assets/Works/MKH/MKH_Scripts/MKH_PlayerEntry.cs
+++ Assets/Works/MKH/MKH_Scripts/MKH_PlayerEntry.cs
@@ -9,7 +9,7 @@ public class MKH_PlayerEntry : MonoBehaviour
     [SerializeField] TMP_Text readyText;
     [SerializeField] TMP_Text nameText;
     [SerializeField] Button readyButton;
-    //[SerializeField] GameObject player;
+    [SerializeField] GameObject play;
 
     public void SetPlayer(Player player)
     {
@@ -25,6 +25,8 @@ public class MKH_PlayerEntry : MonoBehaviour
         {
             nameText.text = player.NickName;
         }
+
+        play.SetActive(true);
         // 레디버튼 활성화
         readyButton.gameObject.SetActive(true);
         // 나 자신이면 레디버튼 클릭 가능
@@ -44,7 +46,7 @@ public class MKH_PlayerEntry : MonoBehaviour
         readyText.text = "";
         nameText.text = "None";
         readyButton.gameObject.SetActive(false);
-        //player.SetActive(false);
+        play.SetActive(false);
     }
 
     public void Ready()
