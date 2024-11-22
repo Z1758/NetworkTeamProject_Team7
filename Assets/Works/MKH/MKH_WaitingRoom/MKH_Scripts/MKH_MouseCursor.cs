72c707c784dd3e1c49d0b8563d2d1b767cdc37a1
MoonKyoungHun <lsf8355@gmail.com>
2024-11-22 17:33:21 +0900

    Fix : 대기 씬 스크립트 수정
    마우스, 대기 씬UI, 대기 씬 스크립트 수정

--- Assets/Works/MKH/MKH_WaitingRoom/MKH_Scripts/MKH_MouseCursor.cs
+++ Assets/Works/MKH/MKH_WaitingRoom/MKH_Scripts/MKH_MouseCursor.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class MKH_MouseCursor : MonoBehaviour
 {
+    [SerializeField] GameObject menu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        menu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
+            menu.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
+            Time.timeScale = 0;
+        }
+        else if(Input.GetKeyDown(KeyCode.Q))
+        {
+            menu.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Time.timeScale = 1;
+        }
+        
+    }
+
+    private void Menu()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            menu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
         }
     }
 }
