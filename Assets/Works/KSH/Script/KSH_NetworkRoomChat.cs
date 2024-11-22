e1786d9a05be1cbc2718ce65527749024dc94b81
KimSu-70 <rlatngh4770@naver.com>
2024-11-22 12:26:49 +0900

    Feat : 개인메시지 구현
    1. 전체 메세지 스크립트 내용 수정
    2. 개인 메시지 프로트타입 구현

--- Assets/Works/KSH/Script/KSH_NetworkRoomChat.cs
+++ Assets/Works/KSH/Script/KSH_NetworkRoomChat.cs
@@ -8,12 +8,13 @@ using UnityEngine.UI;
 using TMPro;
 using Photon.Chat.Demo;
 
-public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
+public class KSH_NetworkRoomChat : MonoBehaviour, IChatClientListener
 {
     private ChatClient _chatClient;  // Photon Chat 클라이언트 객체
 
     private string _userName;        // 사용자 이름
     private string _currentChannelName;  // 현재 채팅 채널 이름
+    private string _privateReceiver = "";    // 개인 메시지 수신자
     [SerializeField] TMP_InputField _inputField;  // 유저가 메시지를 입력하는 InputField UI 요소
     [SerializeField] Text _outputText;        // 메시지가 출력되는 Text UI 요소
 
@@ -36,7 +37,7 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
         _chatID = PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat;
 
         // 기본 채널 설정
-        _currentChannelName = "Channel 001";
+        _currentChannelName = "Channel 002";
 
         // ChatClient 생성, IChatClientListener를 `this`로 전달
         _chatClient = new ChatClient(this);
@@ -62,15 +63,32 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
         }
     }
 
+    private void Update()
+    {
+        // 서버에 연결을 유지하고 지속적으로 호출하여 수신 메세지를 받습니다.
+        _chatClient.Service();
+    }
+
+
+    // 프로그램 종료 시, 서버와의 연결을 끊기
+    public void OnApplicationQuit()
+    {
+        if (_chatClient != null)
+        {
+            _chatClient.Disconnect();  // 애플리케이션 종료 시, 서버와의 연결을 끊습니다.
+        }
+    }
+
+
     public void AddLine(string lineString)
     {
         _outputText.text += lineString + "\r\n";  // outputText에 메시지를 추가
     }
 
-    private void Update()
+    // 개인 메시지를 받을 수신자를 설정
+    public void ReceiverOnValueChange(string valueIn)
     {
-        // 서버에 연결을 유지하고 지속적으로 호출하여 수신 메세지를 받습니다.
-        _chatClient.Service();
+        _privateReceiver = valueIn; // 입력된 값을 privateReceiver에 저장
     }
 
 
@@ -80,20 +98,37 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
         // 연결 상태가 'ConnectedToFrontEnd'일 때만 메시지 전송 가능
         if (_chatClient.State == ChatState.ConnectedToFrontEnd)
         {
+            SubmitPublicChatOnClick();
+            SubmitPrivateChatOnClick();
+        }
+    }
+
+
+    // 전체 채팅을 제출하는 함수
+    public void SubmitPublicChatOnClick()
+    {
+
+        // 개인 메시지가 아니라면 공개 채팅으로 처리
+        if (_privateReceiver == "")
+        {
             // 채널에 메시지를 전송
             _chatClient.PublishMessage(_currentChannelName, _inputField.text);
 
             // 메시지 입력 필드 초기화
             _inputField.text = "";
         }
+
     }
 
-    // 프로그램 종료 시, 서버와의 연결을 끊기
-    public void OnApplicationQuit()
+
+    // 개인 채팅을 제출하는 함수
+    public void SubmitPrivateChatOnClick()
     {
-        if (_chatClient != null)
+
+        if (_privateReceiver != "") // 수신자가 지정되어 있으면
         {
-            _chatClient.Disconnect();  // 애플리케이션 종료 시, 서버와의 연결을 끊습니다.
+            _chatClient.SendPrivateMessage(_privateReceiver, _inputField.text); // 해당 수신자에게 개인 메시지 전송
+            _inputField.text = ""; // 입력 필드 초기화
         }
     }
 
@@ -125,11 +160,13 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
     // 서버에 연결되었을 때 호출되는 콜백
     public void OnConnected()
     {
-        Debug.Log("연결");
-        AddLine(" 서버에 연결되었습니다.");  // 연결 성공 메시지 출력
+        AddLine("서버에 연결되었습니다.");  // 연결 성공 메시지 출력
+
+        // 채팅 채널을 Photon 방 이름으로 설정
+        _currentChannelName = PhotonNetwork.CurrentRoom.Name;
 
         // 연결된 후, 채널에 가입
-        _chatClient.Subscribe(new string[] { _currentChannelName }, 0);  // 채널에 가입
+        _chatClient.Subscribe(new string[] { _currentChannelName }, 10);  // 채널에 가입
     }
 
 
@@ -137,9 +174,10 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
     // 서버에 연결이 끊어졌을 때 호출되는 콜백
     public void OnDisconnected()
     {
+        Debug.Log("연결 안됨");
         // 연결 끊어진 이유 확인을 위한 로그 추가
         Debug.Log("디스커넥트 이유: " + _chatClient.DebugOut.ToString());
-        AddLine(" 서버에 연결이 끊어졌습니다.");  // 연결 끊어짐 메시지 출력
+        AddLine("서버에 연결이 끊어졌습니다.");  // 연결 끊어짐 메시지 출력
     }
 
 
@@ -160,6 +198,7 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
     public void OnPrivateMessage(string sender, object message, string channelName)
     {
         Debug.Log("OnPrivateMessage : " + message);  // 개인 메시지 내용 로그로 출력
+        AddLine(string.Format("<color=magenta>{0} : {1}</color>", sender, message.ToString()));
     }
 
 
@@ -176,7 +215,7 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
     // 채널 구독이 완료되었을 때 호출되는 콜백 channels 구독한 채널 이름 results는 구독 성공 여부
     public void OnSubscribed(string[] channels, bool[] results)
     {
-        AddLine(string.Format(" 채널 입장 ({0})", string.Join(",", channels)));  // 채널에 입장한 후, 채널 이름 출력
+        AddLine(string.Format("채널 입장 ({0})", string.Join(",", channels)));  // 채널에 입장한 후, 채널 이름 출력
     }
 
 
@@ -184,7 +223,7 @@ public class KSH_NetworkChat : MonoBehaviour, IChatClientListener
     // 채널 구독이 취소되었을 때 호출되는 콜백
     public void OnUnsubscribed(string[] channels)
     {
-        AddLine(string.Format(" 채널 퇴장 ({0})", string.Join(",", channels)));  // 채널에서 나갈 때, 퇴장한 채널 이름 출력
+        AddLine(string.Format("채널 퇴장 ({0})", string.Join(",", channels)));  // 채널에서 나갈 때, 퇴장한 채널 이름 출력
     }
 
 
