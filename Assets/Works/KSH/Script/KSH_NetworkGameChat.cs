e510b785c60a6b2fc50c09305398d22f15031b12
KimSu-70 <rlatngh4770@naver.com>
2024-11-26 16:47:53 +0900

    Fix : 2차 수정 완료
    1. 전체적인 코드 개선
    2. 테스트를 위해 Resources-GameObject에 TestGameChat 프리팹 생성

--- Assets/Works/KSH/Script/KSH_NetworkGameChat.cs
+++ Assets/Works/KSH/Script/KSH_NetworkGameChat.cs
@@ -13,6 +13,8 @@ public class KSH_NetworkGameChat : MonoBehaviourPun, IChatClientListener, IPunOb
     private string _userName;        // 사용자 이름
     private string _currentChannelName;  // 현재 채팅 채널 이름
     private string _privateReceiver = "";    // 개인 메시지 수신자
+    private bool isWhispering = false;  // 귓말 모드인지 여부
+    [SerializeField] TMP_InputField _privateInputField; // 개인 메시지 대상자 이름 입력
 
     private PhotonView myView;
 
@@ -82,7 +84,7 @@ public class KSH_NetworkGameChat : MonoBehaviourPun, IChatClientListener, IPunOb
         // 서버에 연결을 유지하고 지속적으로 호출하여 수신 메세지를 받습니다.
         _chatClient.Service();
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (photonView.IsMine && Input.GetKeyDown(KeyCode.Return))
         {
             if (_networkChat.activeSelf)
             {
@@ -114,6 +116,12 @@ public class KSH_NetworkGameChat : MonoBehaviourPun, IChatClientListener, IPunOb
                 _inputField.ActivateInputField(); // 입력 필드 활성화
                 _inputField.Select(); // 입력 필드에 포커스 설정
             }
+        }
+
+        // 탭 키를 눌러 입력란 전환
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInputField();
         }
     }
 
@@ -323,5 +331,23 @@ public class KSH_NetworkGameChat : MonoBehaviourPun, IChatClientListener, IPunOb
         {
             _speechBubbleText.text = (string)stream.ReceiveNext();  // 채팅 메시지 수신
         }
+    }
+
+    // 채팅 입력란과 귓말 입력란을 전환
+    private void ToggleInputField()
+    {
+        if (isWhispering)
+        {
+            // 귓말 모드에서 채팅 모드로 전환
+            _inputField.Select(); // 채팅 입력란 포커스
+        }
+        else
+        {
+            // 채팅 모드에서 귓말 모드로 전환
+            _privateInputField.Select(); // 귓말 대상자 입력란 포커스
+        }
+
+        // 귓말 모드 여부 토글
+        isWhispering = !isWhispering;
     }
 }
