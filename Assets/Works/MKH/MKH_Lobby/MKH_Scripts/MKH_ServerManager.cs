94a54a856b224a69249518a1f4c8a0286f5e87a6
MoonKyoungHun <lsf8355@gmail.com>
2024-11-26 17:16:21 +0900

    Fix : 스크립트 이름 변경
    MKH_LoginScene -> MKH_ServerManager

--- Assets/Works/MKH/MKH_Lobby/MKH_Scripts/MKH_ServerManager.cs
+++ Assets/Works/MKH/MKH_Lobby/MKH_Scripts/MKH_ServerManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MKH_LoginScene : MonoBehaviourPunCallbacks
+public class MKH_ServerManager : MonoBehaviourPunCallbacks
 {
     public enum Panel { Login, Server, List }
 
