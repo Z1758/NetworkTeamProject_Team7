a5ff7ce0662d759786cd42eed9b2d9990f548e96
MoonKyoungHun <lsf8355@gmail.com>
2024-11-26 17:04:02 +0900

    Fix : 로그인씬 스크립트 수정
    리스트 패널 추가
    로그인 이후 메인, 로비 연결 추가
    서버 생성에 관한 연결 추가

--- Assets/Works/MKH/MKH_Lobby/MKH_Scripts/MKH_LoginScene.cs
+++ Assets/Works/MKH/MKH_Lobby/MKH_Scripts/MKH_LoginScene.cs
@@ -6,18 +6,31 @@ using UnityEngine;
 
 public class MKH_LoginScene : MonoBehaviourPunCallbacks
 {
-    public enum Panel { Login, Server }
+    public enum Panel { Login, Server, List }
 
     [SerializeField] MKH_LoginPanel loginPanel;
     [SerializeField] MKH_ServerPanel serverPanel;
+    [SerializeField] MKH_ServerListPanel listPanel;
 
     private void Start()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
 
-        if (PhotonNetwork.IsConnected)
+        if (PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.LoadLevel("MKH_WaitingScene");
+        }
+        else if (PhotonNetwork.InLobby)
+        {
+            SetActivePanel(Panel.List);
+        }
+        else if (PhotonNetwork.IsConnected)
         {
             SetActivePanel(Panel.Server);
+        }
+        else
+        {
+            SetActivePanel(Panel.Login);
         }
     }
 
@@ -34,19 +47,54 @@ public class MKH_LoginScene : MonoBehaviourPunCallbacks
         SetActivePanel(Panel.Login);
     }
 
+    public override void OnCreatedRoom()        // 방 생성 성공 시
+    {
+        Debug.Log("서버 생성 성공");
+    }
+
     public override void OnJoinedRoom()
     {
+        Debug.Log("서버 입장 성공");
         PhotonNetwork.LoadLevel("MKH_WaitingScene");
     }
 
     public override void OnJoinRoomFailed(short returnCode, string message)         // 방 입장 실패 시
     {
-        Debug.Log($"방 입장 실패, 사유 : {message}");
+        Debug.Log($"서버 입장 실패, 사유 : {message}");
+        SetActivePanel(Panel.Server);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)       // 랜덤 방 입장 실패 시
+    {
+        Debug.Log($"랜덤 매칭 실패, 사유 : {message}");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비 입장 성공");
+        SetActivePanel(Panel.List);
+    }
+
+    public override void OnLeftLobby()
+    {
+        Debug.Log("로비 퇴장 성공");
+        listPanel.ClearServerEntries();          // 딕셔너리 방 목록 삭제
+        SetActivePanel(Panel.Server);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> serverList)
+    {
+        // 방의 목록이 변경이 있는 경우 서버에서 보내는 정보들
+        // 주의 사항
+        // 1. 처음 로비 입장 시 : 모든 방 목록을 전달
+        // 2. 입장 중 방 목록이 변경되는 경우 : 변경된 방 목록만 전달
+        listPanel.UpdateServerList(serverList);
     }
 
     private void SetActivePanel(Panel panel)
     {
         loginPanel.gameObject.SetActive(panel == Panel.Login);
         serverPanel.gameObject.SetActive(panel == Panel.Server);
+        listPanel.gameObject.SetActive(panel == Panel.List);
     }
 }
