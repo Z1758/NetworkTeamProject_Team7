3eecd7a248b4d7f7644eb900047a85238267adf4
KimSu-70 <rlatngh4770@naver.com>
2024-11-28 16:43:48 +0900

    Feat : Voice 관련 스크립트 생성 및 수정

---
+++ Assets/Works/KSH/Script/KSH_VoiceStatus.cs
@@ -0,0 +1,16 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using Photon.Voice.PUN;
+
+//public class KSH_VoiceStatus : MonoBehaviour
+//{
+//    [SerializeField] Image _voiceStatusImage;
+//    private PhotonVoiceView _photonVoiceView;
+
+//    void Update()
+//    {
+//        _voiceStatusImage.enabled = this._photonVoiceView.IsSpeaking;
+//    }
+//}
